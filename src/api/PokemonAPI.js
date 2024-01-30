cf80bba58a2fd4dcb6af74681035f619fd05b074
François Philippon <franswa56@hotmail.fr>
2024-01-30 20:24:48 +0100

    start

--- src/api/PokemonAPI.js
+++ src/api/PokemonAPI.js
@@ -6,7 +6,7 @@ const fetchData = async () => {
   try {
     const response = await axios.get('https://tyradex.vercel.app/api/v1/pokemon'); 
     const data = response.data;
-    return data;
+    return data; 
   } catch (error) {
     console.error("Error fetching data: ", error);
     return null;
