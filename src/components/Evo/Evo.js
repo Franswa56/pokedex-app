176d8cbc7934e40cb7d828fd61d27e866018d353
François Philippon <franswa56@hotmail.fr>
2024-02-27 01:41:43 +0100

    conditions evolution

--- src/components/Evo/Evo.js
+++ src/components/Evo/Evo.js
@@ -10,19 +10,15 @@ const Evo = ({ evo }) => {
     // Ne procédez que si evo.evolution n'est pas null
     if (evo.evolution) {
       const fetchPreEvolutions = async () => {
-        const preEvos = await Promise.all(
-          (evo.evolution.pre ?? []).map(pre => fetchPoke(pre.pokedexId))
-        );
+        const preEvos = await Promise.all((evo.evolution.pre ?? []).map((pre) => fetchPoke(pre.pokedexId)));
         setPreEvolutions(preEvos.filter(Boolean));
       };
-  
+
       const fetchNextEvolutions = async () => {
-        const nextEvos = await Promise.all(
-          (evo.evolution.next ?? []).map(next => fetchPoke(next.pokedexId))
-        );
+        const nextEvos = await Promise.all((evo.evolution.next ?? []).map((next) => fetchPoke(next.pokedexId)));
         setNextEvolutions(nextEvos.filter(Boolean));
       };
-  
+
       fetchPreEvolutions();
       fetchNextEvolutions();
     } else {
@@ -32,16 +28,37 @@ const Evo = ({ evo }) => {
     }
   }, [evo]);
 
+
   return (
     <div className="evo">
       {evo.evolution && (
         <>
-          {/* Logique pour les pré-évolutions (identique à votre code actuel) */}
+          {/* Logique pour les pré-évolutions*/}
+
+          {preEvolutions.length > 1 && (
+            <div className="evo__pre">
+              <div key={preEvolutions[0].id}>
+                <h3 className="evo__name">{preEvolutions[0].name.fr}</h3>
+                <img src={preEvolutions[0].sprites.regular} className="evo__image" />
+              </div>
+              <div className="evo__arrow">
+                <span>{evo.evolution.pre[0].condition}</span>
+              </div>
+              <div key={preEvolutions[preEvolutions.length - 1].id}>
+                <h3 className="evo__name">{preEvolutions[preEvolutions.length - 1].name.fr}</h3>
+                <img src={preEvolutions[preEvolutions.length - 1].sprites.regular} className="evo__image" />
+              </div>
+            </div>
+          )}
+
           {preEvolutions.length > 0 && (
             <div className="evo__pre">
               <div key={preEvolutions[preEvolutions.length - 1].id}>
                 <h3 className="evo__name">{preEvolutions[preEvolutions.length - 1].name.fr}</h3>
                 <img src={preEvolutions[preEvolutions.length - 1].sprites.regular} className="evo__image" />
+              </div>
+              <div className="evo__arrow">
+              {evo.evolution.pre[0] ? <span>{evo.evolution.pre[0].condition}</span> : null}
               </div>
               <div className="evo__actual">
                 <h3 className="evo__name">{evo.name.fr}</h3>
@@ -49,20 +66,7 @@ const Evo = ({ evo }) => {
               </div>
             </div>
           )}
-  
-          {preEvolutions.length > 1 && (
-            <div className="evo__pre">
-              <div key={preEvolutions[0].id}>
-                <h3 className="evo__name">{preEvolutions[0].name.fr}</h3>
-                <img src={preEvolutions[0].sprites.regular} className="evo__image" />
-              </div>
-              <div key={preEvolutions[preEvolutions.length - 1].id}>
-                <h3 className="evo__name">{preEvolutions[preEvolutions.length - 1].name.fr}</h3>
-                <img src={preEvolutions[preEvolutions.length - 1].sprites.regular} className="evo__image" />
-              </div>
-            </div>
-          )}
-  
+
           {/* Logique adaptée pour les prochaines évolutions */}
           {nextEvolutions.length > 0 && (
             <div className="evo__next">
@@ -70,18 +74,24 @@ const Evo = ({ evo }) => {
                 <h3 className="evo__name">{evo.name.fr}</h3>
                 <img src={evo.sprites.regular} className="evo__image" />
               </div>
+              <div className="evo__arrow">
+                <span>{evo.evolution.next[0].condition}</span>
+              </div>
               <div key={nextEvolutions[0].id}>
                 <h3 className="evo__name">{nextEvolutions[0].name.fr}</h3>
                 <img src={nextEvolutions[0].sprites.regular} className="evo__image" />
               </div>
             </div>
           )}
-  
+
           {nextEvolutions.length > 1 && (
             <div className="evo__next">
               <div key={nextEvolutions[0].id}>
                 <h3 className="evo__name">{nextEvolutions[0].name.fr}</h3>
                 <img src={nextEvolutions[0].sprites.regular} className="evo__image" />
+              </div>
+              <div className="evo__arrow">
+                <span>{evo.evolution.next[1].condition}</span>
               </div>
               <div key={nextEvolutions[1].id}>
                 <h3 className="evo__name">{nextEvolutions[1].name.fr}</h3>
@@ -93,5 +103,5 @@ const Evo = ({ evo }) => {
       )}
     </div>
   );
-          }
+};
 export default Evo;
