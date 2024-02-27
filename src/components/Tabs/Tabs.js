5c8e017e43ddae271c4c38a2fd98ee47800f6a26
François Philippon <franswa56@hotmail.fr>
2024-02-27 02:00:10 +0100

    ajout transitions onglets

--- src/components/Tabs/Tabs.js
+++ src/components/Tabs/Tabs.js
@@ -1,30 +1,42 @@
 import Tab from "../Tab/Tab";
 import {useState} from 'react'
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import './_Tabs.scss'
 const Tabs = ({ tabs }) => {
     const [activeTab, setActiveTab] = useState(tabs[0].title);
+    const [prevTab, setPrevTab] = useState('');
 
-    console.log("Onglet actif :", activeTab);
-  
-    return (
-      <div className="tabs">
-        <div>
-          {tabs.map(tab => (
-            <Tab
-              key={tab.title}
-              title={tab.title}
-              isActive={tab.title === activeTab}
-              onClick={() => {
-                setActiveTab(tab.title);
-              }}
-            />
-          ))}
-        </div>
-        <div className="tab-content">
-          {tabs.find(tab => tab.title === activeTab).content}
-        </div>
-      </div>
-    );
+    const changeTab = (newTab) => {
+      setPrevTab(activeTab);
+      setActiveTab(newTab);
   };
 
-  export default Tabs
+  
+  return (
+    <div className="tabs">
+      <div>
+        {tabs.map(tab => (
+          <Tab
+            key={tab.title}
+            title={tab.title}
+            isActive={tab.title === activeTab}
+            onClick={() => changeTab(tab.title)}
+          />
+        ))}
+      </div>
+      <TransitionGroup className="tab-content-wrapper">
+        <CSSTransition
+          key={activeTab}
+          timeout={300}
+          classNames="slide"
+        >
+          <div className="tab-content">
+            {tabs.find(tab => tab.title === activeTab).content}
+          </div>
+        </CSSTransition>
+      </TransitionGroup>
+    </div>
+  );
+};
+
+export default Tabs;
